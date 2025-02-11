7d2667c5273cfdb7188ca40b1a821427d20d4630
Girdhari Thakur <girdharikumar101@gmail.com>
2025-02-11 16:35:54 +0530

    fixes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = ({ title, description }) => {
   return (
-    <div className="dark:bg-[#171717] bg-white shadow-lg rounded-lg p-4 m-4 min-w-110 min-h-70 z-10">
-      <h2 className="text-lg font-bold mb-2 text-center text-gray-900 dark:text-gray-100">{title}</h2>
+    <div className="dark:bg-[#171717] bg-white shadow-lg rounded-lg p-4 m-4 w-full max-w-md min-h-70">
+      <h2 className="text-lg font-bold mb-2 text-center text-gray-900 dark:text-white">{title}</h2>
       <p className="text-gray-600 text-center dark:text-gray-300">{description}</p>
     </div>
   );
